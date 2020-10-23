8665d7bd555054c4543a853c687b94b1f5f1430f
Zorawar <44931523+zorofrizzy@users.noreply.github.com>
2020-10-23 13:26:17 +0530

    Delete led.sh

--- NsicShells/led.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo Exporting pin $1. #
-echo $1 > /sys/class/gpio/export #
-echo Setting as Output
-echo out > /sys/class/gpio/gpio$1/direction #
-echo Setting Pin HIGH
-echo 0 > /sys/class/gpio/gpio$1/value
